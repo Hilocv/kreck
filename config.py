085e0246b089d3acf3dd5c7df84956d3e2665c4d
Hilocv <101907782+Hilocv@users.noreply.github.com>
2022-09-17 11:21:38 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = os.environ.get('bot_token','')
-TG_API_ID = os.environ.get('tg_api_id',0)
-TG_API_HASH = os.environ.get('tg_api_hash','')
-TG_ADMIN_USER = os.environ.get('tg_admin_user','')
+BOT_TOKEN = os.environ.get('5442203153:AAFBzpQ4U7kazzTuXX-zn2jYzmrIuoHI0sc','')
+TG_API_ID = os.environ.get('14681595',0)
+TG_API_HASH = os.environ.get('a86730aab5c59953c424abb4396d32d5','')
+TG_ADMIN_USER = os.environ.get('nautaii','')
 ROOT_DIR = 'root'
